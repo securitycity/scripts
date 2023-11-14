4f399731733d22de5c8568a88af76c270c284328
Security City <deetu4200@gmail.com>
2023-11-14 10:05:00 +0300

    Add files via upload

---
+++ scripts/starter.ps1
@@ -0,0 +1,16 @@
+﻿#Schedule script
+
+for(;;) {
+ try {
+  # invoke the worker script
+  $PathScript = pwd
+  powershell C:\serversize.ps1 -WindowStyle Hidden
+
+ }
+ catch {
+  Write-Output "Error"
+ }
+
+ # Time Interval to wait in Seconds
+ Start-Sleep 60
+}
